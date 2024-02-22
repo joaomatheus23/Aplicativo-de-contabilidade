00068303d7d022004de9d612bf536fbd5508ea8e
João Matheus Marques <100247222+joaomatheus23@users.noreply.github.com>
2024-02-22 13:59:20 -0300

    Modificação para empresa Orc Burger

--- MyFinancesSytem/src/App.tsx
+++ MyFinancesSytem/src/App.tsx
@@ -1,26 +1,27 @@
-import { useState } from 'react'
-import './App.css'
-import FinanceControl from './components/FinanceControl/FinanceControl'
-import Header from "./components/Header/Header"
+import React from 'react';
+import { useState } from 'react';
+import './App.css';
+
+import FinanceControl from './components/FinanceControl/FinanceControl';
+import Header from "./components/Header/Header";
 import { Movement } from './components/Models/Interfaces/Movement/Movement';
 import Movements from './components/Movements/Movements';
 import { FormatMoney } from './utils/util';
 
 function App() {
   const [currentBalance, setCurrentBalance] = useState(0);   //State de saldo atual
-  const [currentExpenses, setCurrentExpenses] = useState(0)  //State de despesas atual
+  const [currentExpenses, setCurrentExpenses] = useState(0);  //State de despesas atual
   const [movementsItens, setMovimentsItens] = useState<Array<Movement>>([]); //State de movimentação 
   
   const setNewMovement = (movement: Movement) => {
     if(movement) {
-      setMovimentsItens((prevMovements) =>{
+      setMovimentsItens((prevMovements) => {
         const movements = [... prevMovements];
         movements.unshift({
           name: movement.name,
           value: FormatMoney(movement.value),
           type: movement.type,
           id: Math.random().toString(),
-
         });
         return movements;
       });
@@ -29,7 +30,6 @@ function App() {
       setCurrentBalance(
         (prevBalance) => prevBalance + Number(movement.value)
       );
-
 
       if(movement.type === "Output") {
         setCurrentExpenses(
@@ -41,18 +41,19 @@ function App() {
           (prevBalance) => prevBalance - Number(movement.value)
         );
       }
-      
-
     }
   };
 
-    return (
-      <div> 
-        <Header/>
-        <FinanceControl balance={currentBalance} expense={currentExpenses} handleSetMovement={setNewMovement} />
-        <Movements movementList={movementsItens} />
-      </div>
+  return (
+    <div>
+      <Header/>
+      <FinanceControl balance={currentBalance} expense={currentExpenses} handleSetMovement={setNewMovement} />
+      <Movements movementList={movementsItens} />
+    </div>
   );
 }
 
-export default App
+
+
+
+export default App;
--- MyFinancesSytem/src/components/Header/Header.tsx
+++ MyFinancesSytem/src/components/Header/Header.tsx
@@ -1,15 +1,19 @@
+import React from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSackDollar} from "@fortawesome/free-solid-svg-icons";
-import "./Header.css"
+import "./Header.css";
+import logo from '../../assets/logo.jpeg'; // Importe o logotipo
 
 const Header = () => {
-    return(
+    return (
         <header className="header_container">
-            <FontAwesomeIcon icon={faSackDollar} color="#7AF1a7" size="2x" />
-            <h2> My Finances </h2>
+            <img src={logo} alt="Logo" className="logo" /> {/* Adiciona a imagem */}
+            <div className="title_container">
+                <FontAwesomeIcon icon={faSackDollar} color="#7AF1a7" size="2x" />
+                <h2> Sistema Financeiro Orc Burger</h2>
+            </div>
         </header>
-    )
+    );
 };
 
-
-export default Header
+export default Header;
